699932280c856deb0c9139e176fd16d17ee31ec3
cedriclehmann <cedric.lehmann@mlcrypt.de>
2020-09-11 13:38:55 +0200

    AccessControl
    OpenZeppelin AccessControl for minting and pausing integrated

--- contracts/SIToken.sol
+++ contracts/SIToken.sol
@@ -1,35 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
+import "openzeppelin-solidity/contracts//access/AccessControl.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/utils/Pausable.sol";
 //import "openzeppelin-solidity/contracts/token/ERC20/ERC20Snapshot.sol";
 
-contract SIToken is ERC20, Pausable {
-    address _network;
+contract SIToken is Context, AccessControl, ERC20, Pausable {
 
-    modifier isNetwork() {
-        require(msg.sender == _network, "Not allowed!");
-        _;
-    }
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
-    constructor () public ERC20("Social Impact Token", "SIT") Pausable() {
+
+    constructor () public AccessControl() ERC20("Social Impact Token", "SIT") Pausable()  {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
+
         _mint(msg.sender, 1);
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override{
         super._beforeTokenTransfer(from, to, amount);
-        require(!paused(), "token transfer while paused not possible");
+        require(!paused(), "SIToken: token transfer while paused not possible");
     }
 
-    function pause() isNetwork whenNotPaused public {
-        require(msg.sender == _network, "Not allowed!");
+    function pause() whenNotPaused public {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "SIToken: must have pauser role to pause");
         _pause();
     }
 
-    function unpause() isNetwork whenPaused public {
-        require(msg.sender == _network, "Not allowed!");
+    function unpause() whenPaused public {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "SIToken: must have pauser role to pause");
         _unpause();
     }
 
+    function mint(address to, uint256 amount) public {
+        require(hasRole(MINTER_ROLE, _msgSender()), "SIToken: must have minter role to mint");
+        _mint(to, amount);
+    }
 }
