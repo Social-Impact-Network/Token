5d9d9b71c67bfc242055a95e4a4882bc49dd5d29
cedriclehmann <cedric.lehmann@mlcrypt.de>
2020-09-11 13:01:08 +0200

    Update SIToken.sol
    Pause function added - Token transferability can be paused by the Network Contract

--- contracts/SIToken.sol
+++ contracts/SIToken.sol
@@ -2,15 +2,34 @@
 pragma solidity ^0.6.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Snapshot.sol";
+import "openzeppelin-solidity/contracts/utils/Pausable.sol";
+//import "openzeppelin-solidity/contracts/token/ERC20/ERC20Snapshot.sol";
 
-contract SIToken is ERC20, ERC20Pausable {
+contract SIToken is ERC20, Pausable {
+    address _network;
 
-    /**
-     * @dev Constructor that gives msg.sender all of existing tokens.
-     */
-    constructor () public ERC20("Social Impact Token", "SIT") {
+    modifier isNetwork() {
+        require(msg.sender == _network, "Not allowed!");
+        _;
+    }
+
+    constructor () public ERC20("Social Impact Token", "SIT") Pausable() {
         _mint(msg.sender, 1);
     }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override{
+        super._beforeTokenTransfer(from, to, amount);
+        require(!paused(), "token transfer while paused not possible");
+    }
+
+    function pause() isNetwork whenNotPaused public {
+        require(msg.sender == _network, "Not allowed!");
+        _pause();
+    }
+
+    function unpause() isNetwork whenPaused public {
+        require(msg.sender == _network, "Not allowed!");
+        _unpause();
+    }
+
 }
